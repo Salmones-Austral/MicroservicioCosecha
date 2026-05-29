3ceaea621bb35e54832d9cc1880f6c353adb1c99
Crainsver <cristobal21rc@gmail.com>
2026-05-29 10:52:03 -0400

    MicroservicioCosecha-commit

---
+++ src/main/java/cl/SalmonesAustral/Cosecha/CosechaApplication.java
@@ -0,0 +1,13 @@
+package cl.SalmonesAustral.Cosecha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosechaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosechaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/SalmonesAustral/Cosecha/CosechaApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.SalmonesAustral.Cosecha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosechaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
